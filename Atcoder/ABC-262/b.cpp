7ea6e71caf29b70b13e690e3693c916b6bf78b6a
ShyamB123 <shyamer3@gmail.com>
2023-08-08 19:01:50 +0530

    solved till D

---
+++ Atcoder/ABC-262/b.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld  long double
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define mod 1000000007
+#define inf 1e18
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<pair<int, int>>
+#define vll vector<pair<ll, ll>>
+#define all(a) a.begin(), a.end()
+#define nl '\n'
+
+using namespace std;
+
+//----------------------DEBUGGER----------------------------------------------------------
+#define dbg(x) cout << #x <<" "; _print(x); cout << endl;
+ 
+void _print(ll t) {cout << t;}
+void _print(int t) {cout << t;}
+void _print(string t) {cout << t;}
+void _print(char t) {cout << t;}
+void _print(ld t) {cout << t;}
+void _print(double t) {cout << t;}
+void _print(ull t) {cout << t;}
+
+ 
+template <class T> void _print(vector<vector<T>> v) {for(int j =0;j<v.size();j++){cout <<nl<<"[ "; for (T i : v[j]){_print(i); cout << " ";} cout << "]";};}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cout << "{"; _print(p.F); cout << ","; _print(p.S); cout << "}";}
+template <class T> void _print(vector <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T> void _print(set <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T> void _print(multiset <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T, class V> void _print(map <T, V> v) {cout << "[ "; for (auto i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T>void _print(vector<vector<vector<T>>> v){for(int k =0;k<v.size();k++){_print(v[k]);}}
+//----------------------------------------------------------------------------------------
+
+
+bool compare(pair<ll, ll> p1, pair<ll, ll> p2)
+{
+    return p1.second < p2.second;
+}
+
+/*
+
+*/
+
+
+
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+
+    vii edge;
+    map<pair<int,int>,bool> vert;
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+
+        edge.pb({u,v});
+        vert[{u,v}] = true;
+    }
+
+    int ans = 0;
+    for(auto it : edge)
+    {
+        int u = it.F;
+        int v = it.S;
+
+        for(int k = v + 1; k <= n;k++)
+        {
+            if(vert.find({v,k}) != vert.end() && vert.find({u,k}) != vert.end())
+            {
+                ans++;
+            }
+        }
+    }
+
+    cout << ans <<nl;
+}
+
+
+int main()
+{
+    fast_io;
+    cout << fixed;
+    cout << setprecision(10);
+
+    solve();
+}
